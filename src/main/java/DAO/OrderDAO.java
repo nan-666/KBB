03e54b1b829ac21fc661228628d6e9c564d6563b
nan-666 <https://github.com/nan-666>
2020-06-18 10:11:23 +0800

    合并成功

--- src/main/java/DAO/OrderDAO.java
+++ src/main/java/DAO/OrderDAO.java
@@ -41,7 +41,7 @@ public class OrderDAO {
 					temp.setImg_1(rs.getString("img_1"));
 					temp.setImg_2(rs.getString("img_2"));
 					temp.setImg_3(rs.getString("img_3"));
-					temp.setState(rs.getInt("state"));
+					temp.setState(rs.getInt("state")+"");
 					rows.add(temp);	
 			    	rs.next();
 				}	
@@ -75,10 +75,10 @@ public class OrderDAO {
 					temp.setMoney(rs.getDouble("money"));
 					temp.setImg_1(rs.getString("img_1"));
 					temp.setImg_2(rs.getString("img_2"));
-					temp.setState(rs.getInt("state"));
-					rows.add(temp);	
+					temp.setState(rs.getInt("state")+"");
+					rows.add(temp);
 			    	rs.next();
-				}	
+				}
 			}
 			return rows;
 		}catch(Exception e){
@@ -87,6 +87,124 @@ public class OrderDAO {
 		}
 	}
 
+	//查询订单状态
+	public String selectState(int id){
+		try{
+			String sql = "select * from `orderstate` where id =?";
+			pst = (PreparedStatement) conn.prepareStatement(sql);
+			pst.setInt(1, id);
+			ResultSet rs = pst.executeQuery();
+
+			if(rs.next()){
+				return rs.getString("state");
+			} else {
+				return null;
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	//搜索任务
+	public ArrayList<Order> searchByWord(String word){
+		try{
+			String sql = "select * from `order` where title like '%"
+					+word
+					+"%' or `describe` like '%"
+					+word
+					+"%' or type like '%"
+					+word
+					+"%'";
+			System.out.print(sql);
+			pst = (PreparedStatement) conn.prepareStatement(sql);
+			//pst.setString(1, type);
+
+			ResultSet rs = pst.executeQuery();
+			ArrayList<Order> rows = new ArrayList<Order>();
+			if(rs.next()){
+				for(int i = 0 ; i<rs.getRow();i++){
+		    		Order temp = new Order();
+		    		temp.setId(rs.getInt("id"));
+		    		temp.setUserid(rs.getInt("userid"));
+					temp.setPhone(rs.getString("phone"));
+					temp.setIcon(rs.getString("icon"));
+					temp.setTime(rs.getString("time"));
+					temp.setAddress(rs.getString("address"));
+					temp.setTitle(rs.getString("title"));
+					temp.setDescribe(rs.getString("describe"));
+					temp.setType(rs.getString("type"));
+					temp.setMoney(rs.getDouble("money"));
+					temp.setImg_1(rs.getString("img_1"));
+					temp.setImg_2(rs.getString("img_2"));
+					temp.setImg_3(rs.getString("img_3"));
+					temp.setState(rs.getInt("state")+"");
+					rows.add(temp);	
+			    	rs.next();
+		    	}
+			}
+			return rows;
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	//根据type查询任务类型id
+	public int selectByType(String type){
+		try{
+			String sql = "select id from `ordertype` where type=?";
+			pst = (PreparedStatement) conn.prepareStatement(sql);
+			pst.setString(1, type);
+			ResultSet rs = pst.executeQuery();
+
+			if(rs.next()){
+				return rs.getInt("id");
+			} else {
+				return 0;
+			}
+
+		}catch(Exception e){
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+	//根据type类型查询任务
+	public ArrayList<Order> selectByTypeId(String type){
+		try{
+			int id = this.selectByType(type);
+			String sql = "select * from `order` where ordertypeid="+id;
+			pst = (PreparedStatement) conn.prepareStatement(sql);
+			ResultSet rs = pst.executeQuery();
+			ArrayList<Order> rows = new ArrayList<Order>();
+			if(rs.next()){
+				for(int i = 0; i < rs.getRow(); i++){
+					Order temp = new Order();
+					temp.setId(rs.getInt("id"));
+					temp.setUserid(rs.getInt("userid"));
+					temp.setPhone(rs.getString("phone"));
+					temp.setIcon(rs.getString("icon"));
+					temp.setTime(rs.getString("time"));
+					temp.setAddress(rs.getString("address"));
+					temp.setTitle(rs.getString("title"));
+					temp.setDescribe(rs.getString("describe"));
+					temp.setType(rs.getString("type"));
+					temp.setMoney(rs.getDouble("money"));
+					temp.setImg_1(rs.getString("img_1"));
+					temp.setImg_2(rs.getString("img_2"));
+					temp.setImg_3(rs.getString("img_3"));
+					temp.setState(rs.getInt("state")+"");
+					rows.add(temp);
+					rs.next();
+				}
+			}
+			return rows;
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
     public int insetOrder(Order order) {
         try{
             String sql = "insert into `order`(`userid`,`phone`,`time`,`address`,`describe`,`type`,`money`,`img_1`,`img_2`,`img_3`,`state`) value ('";
